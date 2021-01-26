2720cf6335cfe9a29ca21ab9f1489729f98b0cac
Xandowski <xandy-moraes@hotmail.com>
2021-01-25 21:29:27 -0300

    Finalizado aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo/>
+        <Widget>
+          <Widget.Header>
+            <h1>Mr.robot</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Vamos ver se você sabe tudo sobre Mr.robot!!</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <h2>Quizes da galera</h2>
+        </Widget>
+      </QuizContainer>
+      <Footer/>
+      <GitHubCorner projectUrl="https://github.com/Xandowski/mrrobot-quiz"/>
+    </QuizBackground>
+  )
 }
